11a219e83cb701e0fe5263da7063c6f5246b28a6
Youri Ya <yy.ainu@gmail.com>
2020-12-23 22:55:58 +0300

    Init project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const rectangle = document.querySelector('.rectangle_poligon')
+const triangle = document.querySelector('.triangle_poligon')
+
+rectangle.addEventListener('click', e => {
+  console.log('click on Rectangle')
+})
+
+triangle.addEventListener('click', e => {
+  console.log('click on Triangle')
+})
